edf005edb80c23cb0307536ad4f4beef24554055
anish-18github <anishgaikar17@gmail.com>
2024-10-14 12:23:13 +0530

    first commit

---
+++ src/main/java/io/ClientBridge/ClientBridgeApplication.java
@@ -0,0 +1,13 @@
+package io.ClientBridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientBridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ClientBridge/ClientBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ClientBridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
